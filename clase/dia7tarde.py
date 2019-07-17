f34835e14bd6e757cdcebd1343e050be9076c00c
Eudelio Carmona <alezcano09@gmail.com>
2019-07-17 23:40:50 +0200

    Archivo Dino

---
+++ clase/dia7tarde.py
@@ -0,0 +1,20 @@
+persona = {"nombre": "Leo", 
+"edad":28, "profecion": "vendedor", 
+"hobby": "Golf", 
+"nacionalidad": "paraguaya"}
+idimas = {"espanhol": "bueno",
+"ingles": "medio",
+ "guarani": "bueno",
+ "portugues": "medio"}
+lista_persona = ["Leo", 27, ""]
+diccionario_vacio = {}
+
+print("diccionario persona:", persona)
+print("lista persona:", lista_persona)
+print(persona,["nombre"])
+print(persona, ["edad"])
+print(persona, ["nacionalidad"])
+print(persona, ["idiomas"])
+
+
+
